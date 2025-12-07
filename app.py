d04711434c55e46311ccdbcf1c7d9bd2d8db2a5b
Tarun Kumar <tarunkumar7494905157@gmail.com>
2025-12-07 17:30:58 +0530

    Fixed static routes for sitemap and robots

--- app.py
+++ app.py
@@ -172,3 +172,24 @@ def robots():
 @app.route('/sitemap.xml')
 def sitemap():
     return send_from_directory(os.path.join(app.root_path), 'sitemap.xml')
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# ---- robots.txt ----
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(".", "robots.txt", mimetype="text/plain")
+
+# ---- sitemap.xml ----
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(".", "sitemap.xml", mimetype="application/xml")
+
+if __name__ == "__main__":
+    app.run()
